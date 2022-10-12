ed8a9f950fb79dba1b041c26b6fa35dbd776858c
Abrahamgarza619 <garz260@gmail.com>
2022-10-12 23:49:12 +0000

    forgot to add planets learn more

--- src/js/component/CardPlanet.js
+++ src/js/component/CardPlanet.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { Context } from "../store/appContext";
 
-export const CardPlanet = ( {name, population, terrain, key, planet} ) => {
+export const CardPlanet = ( {name, population, terrain, key, planet, id } ) => {
     const { actions } = useContext(Context);
 
     return (
-<div className="card" style={{width: "18rem"}}>
+<div className="card" style={{width: "18rem"}} key={key}>
   <img className="card-img-top" src="..." alt="Card image cap" />
   <div className="card-body">
     <h5 className="card-title">Name: {name}</h5>
@@ -16,7 +16,7 @@ export const CardPlanet = ( {name, population, terrain, key, planet} ) => {
     <li className="list-group-item">Terrain: {terrain}</li>
     <button type="button" onClick={() => actions.setFavorite(planet)}>Click Me!</button>
   </ul>
-  <Link className="btn btn-primary" to={name}>learn more!</Link>
+  <Link className="btn btn-primary" to={`/single/${id}`}>learn more!</Link>
   </div>
 </div>
     )
